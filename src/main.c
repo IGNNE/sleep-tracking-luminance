a9774ce2b975212c2b2e2ff87c241822b05deb0e
IGNNE <30634883+IGNNE@users.noreply.github.com>
2020-12-29 16:35:31 +0100

    Experimental flash functions

--- src/main.c
+++ src/main.c
@@ -6,31 +6,104 @@
 #include <delay.h>
 #include <stdio.h>
 #include <stm32l073xx.h>
+#include <stdbool.h>
 
-float VoutArray[] =  { 0.0011498,  0.0033908,   0.011498, 0.041803,0.15199,     0.53367, 1.3689,   1.9068,  2.3};
-float  LuxArray[] =  { 1.0108,     3.1201,  9.8051,   27.43,   69.545,   232.67,  645.11,   73.52,  1000};
+float VoutArray[] = { 0.0011498, 0.0033908, 0.011498, 0.041803, 0.15199,
+		0.53367, 1.3689, 1.9068, 2.3 };
+float LuxArray[] = { 1.0108, 3.1201, 9.8051, 27.43, 69.545, 232.67, 645.11,
+		73.52, 1000 };
 
 /**
  * Slightly modified from https://wiki.seeedstudio.com/Grove-Luminance_Sensor/
  *
  * This code uses MultiMap implementation from http://playground.arduino.cc/Main/MultiMap
  */
-float FmultiMap(float val, float * _in, float * _out, uint8_t size)
-{
-    // take care the value is within range
-    // val = constrain(val, _in[0], _in[size-1]);
-    if (val <= _in[0]) return _out[0];
-    if (val >= _in[size-1]) return _out[size-1];
+float FmultiMap(float val, float * _in, float * _out, uint8_t size) {
+	// take care the value is within range
+	// val = constrain(val, _in[0], _in[size-1]);
+	if (val <= _in[0])
+		return _out[0];
+	if (val >= _in[size - 1])
+		return _out[size - 1];
 
-    // search right interval
-    uint8_t pos = 1;  // _in[0] allready tested
-    while(val > _in[pos]) pos++;
+	// search right interval
+	uint8_t pos = 1;  // _in[0] allready tested
+	while (val > _in[pos])
+		pos++;
 
-    // this will handle all exact "points" in the _in array
-    if (val == _in[pos]) return _out[pos];
+	// this will handle all exact "points" in the _in array
+	if (val == _in[pos])
+		return _out[pos];
 
-    // interpolate in the right segment for the rest
-    return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
+	// interpolate in the right segment for the rest
+	return (val - _in[pos - 1]) * (_out[pos] - _out[pos - 1])
+			/ (_in[pos] - _in[pos - 1]) + _out[pos - 1];
+}
+
+#define DATA_ARRAY_SIZE 3000
+const float * data_base_ptr = (float *) (DATA_EEPROM_BASE);
+float data_array[DATA_ARRAY_SIZE] = data_base_ptr;
+size_t * data_array_pos_ptr = (size_t *) (DATA_EEPROM_BASE
+		+ (DATA_ARRAY_SIZE * sizeof(float)));
+bool * buffer_overflow_ptr = (bool *) (DATA_EEPROM_BASE + DATA_ARRAY_SIZE
+		+ sizeof(size_t));
+
+/**
+ * Lässt Schreib-/Lesezugriff auf Flash zu
+ */
+void flash_init() {
+	while ((FLASH->SR & FLASH_SR_BSY) != 0)
+		;
+	if ((FLASH->PECR & FLASH_PECR_PELOCK) != 0) {
+		FLASH->PEKEYR = FLASH_PEKEY1;
+		FLASH->PEKEYR = FLASH_PEKEY2;
+	}
+}
+
+/**
+ * Speichert einen Wert in einer Art Ringbuffer
+ * @param lum Wert, der gespeichert werden soll
+ */
+void flash_save_value(float lum) {
+	if (*data_array_pos_ptr >= DATA_ARRAY_SIZE) {
+		*data_array_pos_ptr = 0;
+		*buffer_overflow_ptr = true;
+	}
+	data_array[(*data_array_pos_ptr)++] = lum;
+}
+
+/**
+ * Verwirft alle Werte im Flash
+ */
+void flash_reset() {
+	*data_array_pos_ptr = 0;
+	*buffer_overflow_ptr = false;
+}
+
+/**
+ * Gibt alle Werte als ein zusammenhängendes Array zurück
+ *
+ * @param[in,out] val_array Array mit min. ::DATA_ARRAY_SIZE Einträgen zeigen
+ *
+ * @return Die tatsächlich geschriebenen Array-Einträge
+ *
+ * @todo pls test!!1!
+ */
+size_t flash_get_values(float * val_array) {
+	if (!*buffer_overflow_ptr) {
+		// no overflow, just copy the values
+		memcpy(val_array, data_array, *data_array_pos_ptr);
+		return *data_array_pos_ptr;
+	} else {
+		// overflow happened, we need to get a bit more creative to get all values
+		size_t values_before_overflow = DATA_ARRAY_SIZE - (*data_array_pos_ptr);
+		size_t values_after_overflow = (*data_array_pos_ptr);
+		memcpy(val_array + (*data_array_pos_ptr), data_array,
+				values_before_overflow);
+		memcpy(val_array, data_array + values_before_overflow,
+				values_after_overflow);
+		return DATA_ARRAY_SIZE;
+	}
 }
 
 int main(void) {
@@ -39,16 +112,38 @@ int main(void) {
 	i2c_setup();
 	lcd_init();
 	lcd_print_string("    Embedded    "
-	                 "    Systems!    ");
+			"    Systems!    ");
 
 	// set up gpios
 	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
-	GPIOA->MODER &=~(GPIO_MODER_MODE0);
+	GPIOA->MODER &= ~(GPIO_MODER_MODE0);
 
 	adc_init();
 
+	int * data_ptr = (int *) (DATA_EEPROM_BASE);
+
+	// create random number
+	srand(read_adc_raw_blocking(ADC_CHANNEL_0));
+	int current = rand();
+	int last = *data_ptr;
+	*data_ptr = current;
+	char buffer[10];
+
+	lcd_print_string("current: ");
+	snprintf(buffer, sizeof(buffer), "%x", current);
+	lcd_print_string(buffer);
+	lcd_set_cursor(1, 0);
+
+	lcd_print_string("last: ");
+	snprintf(buffer, sizeof(buffer), "%x", last);
+	lcd_print_string(buffer);
+
+	while (1)
+		;
+
 	while (1) {
-		float sensor_voltage = (float)(read_vdda() * read_adc_raw_blocking(ADC_CHANNEL_0)) / (4095 * 1000);
+		float sensor_voltage = (float) (read_vdda()
+				* read_adc_raw_blocking(ADC_CHANNEL_0)) / (4095 * 1000);
 
 		float luminance = FmultiMap(sensor_voltage, VoutArray, LuxArray, 9);
 
